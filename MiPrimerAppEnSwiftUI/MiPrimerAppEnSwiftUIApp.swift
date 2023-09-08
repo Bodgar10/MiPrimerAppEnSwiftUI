131aa69a818207b342d641f7ab1c5968f68b2fd0
Bodgar Jair Espinosa Miranda <bodgar_jair@hotmail.com>
2023-09-07 18:18:24 -0600

    Initial Commit

---
+++ MiPrimerAppEnSwiftUI/MiPrimerAppEnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiPrimerAppEnSwiftUIApp.swift
+//  MiPrimerAppEnSwiftUI
+//
+//  Created by Bodgar Espinosa Miranda on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MiPrimerAppEnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
